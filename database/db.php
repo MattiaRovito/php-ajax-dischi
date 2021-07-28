7b0a7e3c58d5fd2397ad0c779c49f0da8976dd40
MattiaR <rovitomattia91@gmail.com>
2021-07-28 15:35:43 +0200

    fine esercizio + un po' di layout

--- database/db.php
+++ database/db.php
@@ -2,30 +2,83 @@
 
 $songs = [
     [
-        'title' => 'New Jersey',
-        'author' => 'Bon Jovi',
-        'year' => 1988,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+        'title' => 'Bohemian Rapsody',
+        'author' => 'Queen',
+        'year' => 1975,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/61QgeFWLvfL._AC_SY450_.jpg'
     ],
     [
-        'title' => 'New Jersey',
-        'author' => 'Bon Jovi',
-        'year' => 1988,
+        'title' => 'Versace on the Floor',
+        'author' => 'Bruno Mars',
+        'year' => 2016,
         'poster' => 'https://images-na.ssl-images-amazon.com/images/I/716mnZd24EL._AC_SY450_.jpg'
     ],
     [
-        'title' => 'New Jersey',
-        'author' => 'Bon Jovi',
+        'title' => 'Prisoner 709',
+        'author' => 'Caparezza',
         'year' => 1988,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/716mnZd24EL._AC_SY450_.jpg'
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71BCCBEm%2BUL._AC_SY450_.jpg'
     ],
     [
-        'title' => 'New Jersey',
-        'author' => 'Bon Jovi',
+        'title' => 'Highway Tune',
+        'author' => 'Greta van Fleet',
+        'year' => 2017,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/61blyJMUwvL._AC_SY450_.jpg'
+    ],
+    [
+        'title' => 'Via del campo',
+        'author' => 'Fabrizio De Andrè',
+        'year' => 1967,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71RTZYaJ%2B2L._AC_SY450_.jpg'
+    ],
+    [
+        'title' => 'I cento passi',
+        'author' => 'Modena City Ramblers',
+        'year' => 2004,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/61Kx5R93bOL._AC_SY450_.jpg'
+    ],
+    [
+        'title' => 'Mr. Brightside',
+        'author' => 'The Killers',
         'year' => 1988,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/716mnZd24EL._AC_SY450_.jpg'
+        'poster' => 'https://lineimg.omusic.com.tw/img/album/1065577.jpg?v=20200512073858'
+    ],
+    [
+        'title' => 'Thunderstruck',
+        'author' => 'Ac/Dc',
+        'year' => 1990,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81Bpdiv2yLL._AC_SY450_.jpg'
+    ],
+    [
+        'title' => 'Around the World',
+        'author' => 'Daft Punk',
+        'year' => 1997,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51xwLTFiHfL._AC_SY450_.jpg'
+    ],
+    [
+        'title' => 'November Rain',
+        'author' => 'Guns n Roses',
+        'year' => 1992,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71cW4R2bc2L._AC_SY450_.jpg'
+    ],
+    [
+        'title' => 'Never Gonna Give You Up',
+        'author' => 'Rick Astley',
+        'year' => 1987,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/618oaxkgK6L._AC_SX466_.jpg'
     ],
     
+    
+];
+
+
+
+
+$socials = [
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png',
+    'https://upload.wikimedia.org/wikipedia/it/0/09/Twitter_bird_logo.png',
+    'http://www.galiamanfrida.it/wp-content/uploads/2017/10/instagram-Logo-PNG-Transparent-Background-download.png',
+    'https://www.perazza.it/wp-content/uploads/2019/03/linkedin-logo-copy.png'
 ];
 
 
--- index-php.php
+++ index-php.php
@@ -40,7 +40,7 @@ include __DIR__ . '../database/db.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- MY CSS -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
 
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -49,32 +49,56 @@ include __DIR__ . '../database/db.php';
 </head>
 <body>
 
+<header>
+
+    <img src="https://9b16f79ca967fd0708d1-2713572fef44aa49ec323e813b06d2d9.ssl.cf2.rackcdn.com/1140x_a10-7_cTC/NS-WKMAG0730-1595944356.jpg" alt="logo">
+</header>
+
+
+
+
+<main>
     <div class="container">
+
         <div class="row">
             <?php foreach($songs as $song) { ?>
 
-                <div class="col">
-                    <h3>    
+                <div class="col-3 p-3">
+                    <h5>    
                         <?php echo $song['title'] ?>
-                    </h3>
-                    <p>
+                    </h5>
+                    <p class="m-0">
                         <?php echo $song['author'] ?>
                     </p>
                     <p>
                         <?php echo $song['year'] ?>
                     </p>
-                    <img src="<?php echo $song['poster'] ?>" alt="img" class="w-25" >
+                    <img src="<?php echo $song['poster'] ?>" alt="img" class="w-75" >
                 </div>
-                
-                    
-                
-
-
+            
             <?php } ?>
         </div>
 
     </div>
+</main>
     
+    
+
+
+
+<footer>
+    <div class="d-flex justify-content-center" >
+        <h5 class="fw-bold">Seguici sui social!</h5>
+    
+        <?php foreach($socials as $social) { ?>
+        
+        <img src="<?php echo $social ?>" alt="social">
+        
+        <?php } ?>  
+
+    </div> 
+
+</footer>
 
 
 
